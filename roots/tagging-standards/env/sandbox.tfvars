f412934c2df78159a259ad778f5b8137aa9214cf
balutbomber <balutbomber@gmail.com>
2024-10-28 16:57:34 -0700

    update

--- roots/tagging-standards/env/sandbox.tfvars
+++ roots/tagging-standards/env/sandbox.tfvars
@@ -1,3 +1,3 @@
 env ="sbx"
 AWS_REGION ="us-west-2"
-OU = "arn:aws:organizations::582983609964:ou/o-zpqpmip2bd/ou-077q-eszh9m9t"
+OU = "ou-077q-eszh9m9t"
--- roots/tagging-standards/main.tf
+++ roots/tagging-standards/main.tf
@@ -11,21 +11,119 @@ resource "aws_organizations_policy" "tagpolicy" {
   content = <<CONTENT
 {
   "tags": {
-    "Environment": {
+    "DHCS:ProgamContact": {
       "tag_key": {
-        "@@assign": "Environment"
+        "@@assign": "DHCS:ProgamContact"
+      }
+    },
+    "DHCS:InfrastructureContact": {
+      "tag_key": {
+        "@@assign": "DHCS:InfrastructureContact"
+      }
+    },  
+    "DHCS:ApplicationContact": {
+      "tag_key": {
+        "@@assign": "DHCS:ApplicationContact"
+      }
+    },  
+    "DHCS:ApprovalReference": {
+      "tag_key": {
+        "@@assign": "DHCS:ApprovalReference"
+      }
+    },  
+    "DHCS:Description": {
+      "tag_key": {
+        "@@assign": "DHCS:Description"
+      }
+    },  
+    "DHCS:DataClassification": {
+      "tag_key": {
+        "@@assign": "DHCS:DataClassification"
       },
       "tag_value": {
         "@@assign": [
-          "dev",
-          "test",
-          "prod"
+          "PI", "PHI", "SSA", "SENSITIVE", "PUBLIC", "HIPAA", "PII", "Confidential"
         ]
-      }      
-    },
-    "name": {
+      },      
+      "enforced_for": {
+        "@@assign": [
+            "elasticfilesystem:*", "s3:bucket", "ec2:instance", "dynamodb:table"
+        ]
+      }
+    },  
+    "DHCS:Fips199Categorization": {
       "tag_key": {
-        "@@assign": "name"
+        "@@assign": "DHCS:Fips199Categorization"
+      },
+      "tag_value": {
+        "@@assign": [
+          "Low", "Moderate", "High"
+        ]
+      },      
+      "enforced_for": {
+        "@@assign": [
+            "elasticfilesystem:*", "s3:bucket", "ec2:instance", "dynamodb:table"
+        ]
+      }
+    },
+    "DHCS:Encryption": {
+      "tag_key": {
+        "@@assign": "DHCS:Encryption"
+      },
+      "tag_value": {
+        "@@assign": [
+          "Low", "Moderate", "High"
+        ]
+      },      
+      "enforced_for": {
+        "@@assign": [
+            "elasticfilesystem:*", "s3:bucket", "ec2:instance", "dynamodb:table"
+        ]
+      }
+    },  
+    "Name": {
+      "tag_key": {
+        "@@assign": "Name"
+      }
+    },
+    "DHCS:Environment": {
+      "tag_key": {
+        "@@assign": "DHCS:Environment"
+      }
+    },
+    "DHCS:Application": {
+      "tag_key": {
+        "@@assign": "DHCS:Application"
+      }
+    },
+    "DHCS:SourceControl": {
+      "tag_key": {
+        "@@assign": "DHCS:SourceControl"
+      }
+    },
+    "DHCS:IacContext": {
+      "tag_key": {
+        "@@assign": "DHCS:IacContext"
+      }
+    },
+    "DHCS:BackupPolicy": {
+      "tag_key": {
+        "@@assign": "DHCS:BackupPolicy"
+      }
+    },
+    "DHCS:PatchGroup": {
+      "tag_key": {
+        "@@assign": "DHCS:PatchGroup"
+      }
+    },
+    "DHCS:MaintenanceWindow": {
+      "tag_key": {
+        "@@assign": "DHCS:MaintenanceWindow"
+      }
+    },
+    "CostCenter": {
+      "tag_key": {
+        "@@assign": "CostCenter"
       }
     }
   }
